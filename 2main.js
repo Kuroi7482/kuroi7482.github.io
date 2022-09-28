b970ec1827f7061ede01e38996cd0fdacd8a5566
Kuroi7482 <112910080+Kuroi7482@users.noreply.github.com>
2022-09-28 17:33:04 -0500

    Create 2main.js

---
+++ 2main.js
@@ -0,0 +1,12 @@
+const tocarNota = ()=> {
+  // Obtenemos la nota
+  // Reseteamos la ejecución de la nota
+  // Utilizamos el método play de la nota
+  // Añadimos la clase active
+  // Agregamos un event listener al evento ended
+    // Cuando termine de reproducirse el sonido, removemos la clase active
+}
+
+const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
